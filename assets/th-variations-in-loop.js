a4403d00b7fa466a5e3feb9507f7809f5487ed4f
Sämi Will <saemiwill@gmail.com>
2021-03-16 14:37:07 +0100

    style editing and bugfixes

--- assets/th-variations-in-loop.js
+++ assets/th-variations-in-loop.js
@@ -9,11 +9,12 @@ jQuery(function ($) {
         $('.variations_in_loop_thumbnail').on('click', function() {
             var variation_attribute = $(this).data( 'variation-attribute' );
             var old_url = $(this).closest( '.product' ).find('a').attr('href');
+            old_url = old_url.replace(/([?]?attribute_pa_ausfuehrung=)+(\w+)/g, '');
             if ( $(this).hasClass('active') ) {
                 location.href=old_url + "?attribute_pa_ausfuehrung=" + variation_attribute;
             }
 
-            $( '.variations_in_loop_thumbnail' ).removeClass('active');
+            $(this).closest('.product').find( '.variations_in_loop_thumbnail' ).removeClass('active');
             $(this).addClass('active');
 
             // Bild auswechseln
