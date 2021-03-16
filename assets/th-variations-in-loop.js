3c768e8fd606d5a451fc0961a1588809be67543b
Sämi Will <saemiwill@gmail.com>
2021-03-16 13:51:20 +0100

    variatons click only

--- assets/th-variations-in-loop.js
+++ assets/th-variations-in-loop.js
@@ -1,25 +1,33 @@
 jQuery(function ($) {
     "use strict",
     $(document).ready(function () {
-        // Produktbild wechseln bei mouseover
-        $('.variations_in_loop_thumbnail').hover(function() {
-            $(this).toggleClass('active');
 
-            var variation_img_src = $(this).data('variation-img-src');
-            var variation_img_srcset = $(this).data('variation-img-srcset');
-            
-            var product_img = $(this).closest( '.product' ).find( '.wp-post-image' );
-            product_img.attr('src', variation_img_src);
-            product_img.attr('srcset', variation_img_srcset);
-
-        });
+        // Erste Version aktiv
+        $('.variations_in_loop_thumbnail:first-child').addClass('active');
 
         // Richtige Variante Laden bei Klick
         $('.variations_in_loop_thumbnail').on('click', function() {
             var variation_attribute = $(this).data( 'variation-attribute' );
-            var product_url = $(this).closest( '.product' ).find('a').attr('href');
+            var old_url = $(this).closest( '.product' ).find('a').attr('href');
+            if ( $(this).hasClass('active') ) {
+                location.href=old_url + "?attribute_pa_ausfuehrung=" + variation_attribute;
+            }
 
-            location.href=product_url + "?attribute_pa_ausfuehrung=" + variation_attribute;
+            $( '.variations_in_loop_thumbnail' ).removeClass('active');
+            $(this).addClass('active');
+
+            // Bild auswechseln
+            var variation_img_src = $(this).data('variation-img-src');
+            var variation_img_srcset = $(this).data('variation-img-srcset');
+            var product_img = $(this).closest( '.product' ).find( '.wp-post-image' );
+            product_img.attr('src', variation_img_src);
+            product_img.attr('srcset', variation_img_srcset);
+
+            // Alle Links auswechseln
+            var product_links = $(this).closest( '.product' ).find('a');
+            product_links.attr('href', old_url + "?attribute_pa_ausfuehrung=" + variation_attribute);
+
         });
+
     });
 });
