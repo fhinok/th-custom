a3897d8d5c3371b5f9cea3d704d1600977886c6c
Sämi Will <saemiwill@gmail.com>
2021-03-18 14:43:07 +0100

    add to event for ajax

--- assets/th-variations-in-loop.js
+++ assets/th-variations-in-loop.js
@@ -33,39 +33,43 @@ jQuery(function ($) {
         });
 
     // in table
-        // Erste Version aktiv
-        $('.variations_in_table_thumbnail:first-child').addClass('active');
+        function th_variations_in_table() {
+            // Erste Version aktiv
+            $('.variations_in_table_thumbnail:first-child').addClass('active');
 
-        $('.variations_in_table_thumbnail').on('click', function() {
-            var variation_id = $(this).data( 'variation-id' );
-            var variation_attributes = $(this).data( 'variation-attributes' );
-            var variation_url = "";
-            Object.keys(variation_attributes).forEach( function( key ) {
-                variation_url += "?"+key+"="+variation_attributes[key];
+            $('.variations_in_table_thumbnail').on('click', function() {
+                var variation_id = $(this).data( 'variation-id' );
+                var variation_attributes = $(this).data( 'variation-attributes' );
+                var variation_url = "";
+                Object.keys(variation_attributes).forEach( function( key ) {
+                    variation_url += "?"+key+"="+variation_attributes[key];
+                })
+
+                var product_url = $(this).data( 'product-url' );
+                if ( $(this).hasClass('active') ) {
+                    location.href = product_url + variation_url;
+                }
+
+                $(this).closest('.wpt_variation').find( '.variations_in_table_thumbnail' ).removeClass('active');
+                $(this).addClass('active');
+
+                // Produkte ID auswechseln
+                $(this).closest('.wpt_row').data('product_id', variation_id);
+
+                // Bild auswechseln
+                var variation_img_src = $(this).data('variation-img-src');
+                var variation_img_srcset = $(this).data('variation-img-srcset');
+                var product_img = $(this).closest( '.wpt_row' ).find( '.wpt_thumbnails_popup img' );
+                product_img.attr('src', variation_img_src);
+                product_img.attr('srcset', variation_img_srcset);
+                
+                var product_popup = $(this).closest( '.wpt_row' ).find( '.wpt_thumbnails_popup' );
+                product_popup.data( 'url', variation_img_src);
+
             })
-
-            var product_url = $(this).data( 'product-url' );
-            if ( $(this).hasClass('active') ) {
-                location.href = product_url + variation_url;
-            }
-
-            $(this).closest('.wpt_variation').find( '.variations_in_table_thumbnail' ).removeClass('active');
-            $(this).addClass('active');
-
-            // Produkte ID auswechseln
-            $(this).closest('.wpt_row').data('product_id', variation_id);
-
-            // Bild auswechseln
-            var variation_img_src = $(this).data('variation-img-src');
-            var variation_img_srcset = $(this).data('variation-img-srcset');
-            var product_img = $(this).closest( '.wpt_row' ).find( '.wpt_thumbnails_popup img' );
-            product_img.attr('src', variation_img_src);
-            product_img.attr('srcset', variation_img_srcset);
-            
-            var product_popup = $(this).closest( '.wpt_row' ).find( '.wpt_thumbnails_popup' );
-            product_popup.data( 'url', variation_img_src);
-
-        })
-
+        }
+        $(document).on('wc_fragments_refreshed', function() {
+            th_variations_in_table();
+        });
     });
 });
